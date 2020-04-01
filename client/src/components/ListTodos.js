2681d6d1d07109967faa2786d120b1cf67e4fe6e
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-03-31 17:28:04 -0700

    removed comments

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -45,11 +45,6 @@ const ListTodos = () => {
           </tr>
         </thead>
         <tbody>
-          {/*<tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr> */}
           {todos.map(todo => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
